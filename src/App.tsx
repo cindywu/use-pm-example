822ef22d06aff709c1ddde4c9c075154eea52543
cindy <ciindy.wu@gmail.com>
2021-09-13 18:47:09 -1000

    make editor work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import "prosemirror-view/style/prosemirror.css";
+import "./styles.css";
 
-function App() {
+import React from "react";
+import { schema } from "prosemirror-schema-basic";
+import { keymap } from "prosemirror-keymap";
+import { baseKeymap, Command, toggleMark } from "prosemirror-commands";
+import { MarkType } from "prosemirror-model";
+import { history, redo, undo } from "prosemirror-history";
+import useProseMirror from './useProseMirror'
+import ProseMirror from './ProseMirror'
+import { EditorState, Transaction } from "prosemirror-state";
+
+const toggleBold = toggleMarkCommand(schema.marks.strong);
+const toggleItalic = toggleMarkCommand(schema.marks.em);
+
+const opts: Parameters<typeof useProseMirror>[0] = {
+  schema,
+  plugins: [
+    history(),
+    keymap({
+      ...baseKeymap,
+      "Mod-z": undo,
+      "Mod-y": redo,
+      "Mod-Shift-z": redo,
+      "Mod-b": toggleBold,
+      "Mod-i": toggleItalic
+    })
+  ]
+};
+
+export default function App() {
+  const [state, setState] = useProseMirror(opts);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Menu">
+        <Button
+          className="bold"
+          isActive={isBold(state)}
+          onClick={() => toggleBold(state, (tr) => setState(state.apply(tr)))}
         >
-          Learn React
-        </a>
-      </header>
+          B
+        </Button>
+        <Button
+          className="italic"
+          isActive={isItalic(state)}
+          onClick={() => toggleItalic(state, (tr) => setState(state.apply(tr)))}
+        >
+          I
+        </Button>
+      </div>
+      <div className="ProseMirrorContainer">
+        <ProseMirror
+          className="ProseMirror"
+          state={state}
+          onChange={setState}
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+function toggleMarkCommand(mark: MarkType): Command {
+  return (
+    state: EditorState,
+    dispatch: ((tr: Transaction) => void) | undefined
+  ) => toggleMark(mark)(state, dispatch);
+}
+
+function isBold(state: EditorState): boolean {
+  return isMarkActive(state, schema.marks.strong);
+}
+
+function isItalic(state: EditorState): boolean {
+  return isMarkActive(state, schema.marks.em);
+}
+
+// https://github.com/ProseMirror/prosemirror-example-setup/blob/afbc42a68803a57af3f29dd93c3c522c30ea3ed6/src/menu.js#L57-L61
+function isMarkActive(state: EditorState, mark: MarkType): boolean {
+  const { from, $from, to, empty } = state.selection;
+  return empty
+    ? !!mark.isInSet(state.storedMarks || $from.marks())
+    : state.doc.rangeHasMark(from, to, mark);
+}
+
+function Button(props: {
+  children: React.ReactNode;
+  isActive: boolean;
+  className: string;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      className={props.className}
+      style={{
+        backgroundColor: props.isActive ? "#efeeef" : "#fff",
+        color: props.isActive ? "blue" : "black"
+      }}
+      onMouseDown={handleMouseDown}
+    >
+      {props.children}
+    </button>
+  );
+
+  function handleMouseDown(e: React.MouseEvent) {
+    e.preventDefault(); // Prevent editor losing focus
+    props.onClick();
+  }
+}
